4981a540a1e4ad84881701c39bf34c06761a4fe1
RicardoRR29 <r3ifes@gmail.com>
2021-02-24 20:53:53 -0300

    finished first lecture

--- moveitt/src/App.tsx
+++ moveitt/src/App.tsx
@@ -1,9 +1,10 @@
+import { ExperienceBar } from './components/ExperienceBar';
 import './styles/global.css';
 
 function App() {
   return (
-    <div>
-      <button style={{ height: '2rem' }}></button>
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
