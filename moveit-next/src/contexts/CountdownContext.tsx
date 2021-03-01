4377abdad1573cf425b493139b9f9734c7be7319
RicardoRR29 <r3ifes@gmail.com>
2021-02-28 22:47:38 -0300

    ajustei o relogio

--- moveit-next/src/contexts/CountdownContext.tsx
+++ moveit-next/src/contexts/CountdownContext.tsx
@@ -23,7 +23,7 @@ export function CountdownProvider({ children }) {
     const { startNewChallenge } = useContext(ChallengesContext);
 
 
-    const [time, setTime] = useState(0.05 * 60);
+    const [time, setTime] = useState(25 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinish] = useState(false);
 
@@ -38,7 +38,7 @@ export function CountdownProvider({ children }) {
     function resetCountdown() {
         clearTimeout(countdownTimeout)
         setIsActive(false);
-        setTime(0.05 * 60);
+        setTime(25 * 60);
         setHasFinish(false)
     }
 
